495ac0c6fe439bbfa230ac383015c6bbb46b0acd
Daniel Jasek <Dan@Chriss-Mac-mini.local>
2016-08-11 16:27:04 -0400

    auto-add .vimrc

--- vim.sh
+++ vim.sh
@@ -4,3 +4,5 @@ git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab ~/.vim/bundle/supertab
 mkdir ~/.vim/colors && curl -LSso ~/.vim/colors/hybrid.vim https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/hybrid.vim
+
+cp .vimrc ~
